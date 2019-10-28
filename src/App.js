7498b15aaf5683caff2111498c15c5584fa60a6d
Dana Simmons <dsanalog@gmail.com>
2019-10-27 22:22:40 -0400

    Install CanvasJS and get a chart to render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CanvasJSReact from './assets/canvasjs/canvasjs.react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CanvasJSChart = CanvasJSReact.CanvasJSChart
+
+
+const options = {
+  animationEnabled: true,
+  exportEnabled: true,
+  theme: "light2", // "light1", "dark1", "dark2"
+  title:{
+    text: "Sensor Data"
+  },
+  axisY: {
+    title: "Temperature",
+    includeZero: false,
+    suffix: "%"
+  },
+  axisX: {
+    title: "Time",
+    prefix: "W",
+    interval: 2
+  },
+  data: [{
+    type: "line",
+    toolTipContent: "Week {x}: {y}%",
+    dataPoints: []
+  }]
+}
+class App extends Component {
+  state = {
+    samples: [
+      { x: 1, y: 64 },
+      { x: 2, y: 61 },
+      { x: 3, y: 64 },
+      { x: 4, y: 62 },
+      { x: 5, y: 64 },
+      { x: 6, y: 60 },
+    ]
+  }
+  options = {
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "light2", // "light1", "dark1", "dark2"
+    title:{
+      text: "Sensor Data"
+    },
+    axisY: {
+      title: "Temperature",
+      includeZero: false,
+      suffix: "C"
+    },
+    axisX: {
+      title: "Time",
+      prefix: "S",
+      interval: 2
+    },
+    data: [{
+      type: "line",
+      toolTipContent: "Week {x}: {y}%",
+      dataPoints: ( ()=> this.state.samples)()
+    }]
+  }
+  render() {
+    return (
+      <div>
+        <CanvasJSChart options={this.options } />
+      </div>
+    )
+  }
 }
 
 export default App;
