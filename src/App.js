17620106e8634565beabe14b9f09b09a74988ca9
Dana Simmons <dsanalog@gmail.com>
2019-10-27 22:33:40 -0400

    Remove CanvasJS for react-chartjs-2
    CanvasJS is a licensed library, although there is a free version,
    ChartJS seemed simpler with less frill

--- src/App.js
+++ src/App.js
@@ -1,72 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Line } from 'react-chartjs-2';
+
 import './App.css';
-import CanvasJSReact from './assets/canvasjs/canvasjs.react'
-
-const CanvasJSChart = CanvasJSReact.CanvasJSChart
 
 
-const options = {
-  animationEnabled: true,
-  exportEnabled: true,
-  theme: "light2", // "light1", "dark1", "dark2"
-  title:{
-    text: "Sensor Data"
-  },
-  axisY: {
-    title: "Temperature",
-    includeZero: false,
-    suffix: "%"
-  },
-  axisX: {
-    title: "Time",
-    prefix: "W",
-    interval: 2
-  },
-  data: [{
-    type: "line",
-    toolTipContent: "Week {x}: {y}%",
-    dataPoints: []
-  }]
-}
 class App extends Component {
-  state = {
-    samples: [
-      { x: 1, y: 64 },
-      { x: 2, y: 61 },
-      { x: 3, y: 64 },
-      { x: 4, y: 62 },
-      { x: 5, y: 64 },
-      { x: 6, y: 60 },
+
+  data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
     ]
   }
-  options = {
-    animationEnabled: true,
-    exportEnabled: true,
-    theme: "light2", // "light1", "dark1", "dark2"
-    title:{
-      text: "Sensor Data"
-    },
-    axisY: {
-      title: "Temperature",
-      includeZero: false,
-      suffix: "C"
-    },
-    axisX: {
-      title: "Time",
-      prefix: "S",
-      interval: 2
-    },
-    data: [{
-      type: "line",
-      toolTipContent: "Week {x}: {y}%",
-      dataPoints: ( ()=> this.state.samples)()
-    }]
-  }
+
   render() {
     return (
       <div>
-        <CanvasJSChart options={this.options } />
+        <Line data={ this.data } />        
       </div>
     )
   }
